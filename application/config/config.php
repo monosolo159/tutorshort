4e6723af05e229376bf3f6167a7ecf4e3024cba6
esarnstudio <wichetpong159@hotmail.com>
2017-01-18 18:30:44 +0700

    S

--- application/config/config.php
+++ application/config/config.php
@@ -14,7 +14,7 @@
 | path to your installation.
 |
 */
-$config['base_url']	= 'http://localhost/tutorshot/';
+$config['base_url']	= 'http://localhost/tutorshort/';
 
 /*
 |--------------------------------------------------------------------------
